81863786125f1f4193c0bd87b32a968f5fa07245
Ryan Coble <ryan.m.coble@gmail.com>
2016-12-20 19:56:24 -0500

    Fixed issue with ETA when no miners are running.

--- app/models/ETA.php
+++ app/models/ETA.php
@@ -8,13 +8,18 @@ class ETA {
                 $unpaid = (float) substr_replace($unpaid, '.', strlen($unpaid) - 18, 0);
 
                 $minerData->unpaid = $unpaid;
-		
-		$ethBeforePayout = 1.00 - $minerData->unpaid;
-                $mins = $ethBeforePayout / $minerData->ethPerMin;
-                $hours = $mins / 60;
 
-                $etaHours = (int)$hours;
-                $etaMins = (int)(($hours - (int)$hours) * 60);
+                $etaHours = '&infin;';
+                $etaMins  = '&infin;';
+
+                if($minerData->ethPerMin !== 0) {
+                        $ethBeforePayout = 1.00 - $minerData->unpaid;
+                        $mins = $ethBeforePayout / $minerData->ethPerMin;
+                        $hours = $mins / 60;
+
+                        $etaHours = (int)$hours;
+                        $etaMins = (int)(($hours - (int)$hours) * 60);
+                }
 
                 $eta = $etaHours . ' hour(s), ' . $etaMins . ' min(s)';
 
